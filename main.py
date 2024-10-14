911c31abf5d21c8ee668bf0ba0a4e734bf35ed19
GoldSonBR <gugadantas2010@gmail.com>
2024-10-14 13:01:26 -0300

    encryption and decryption func added, UI color revamp

--- main.py
+++ main.py
@@ -3,18 +3,73 @@ from tkinter import messagebox
 import base64
 import os
 
+
 def decrypt():
-    print("")
+    password = code.get()
+
+    if password == "1234":
+        screen2 = Toplevel(screen)
+        screen2.title("decryption")
+        screen2.geometry("400x200")
+        screen2.configure(bg="#06D6A0")
+        image_icon = PhotoImage(file="App_icon.png")
+        screen2.iconphoto(False, image_icon)
+
+        message = text1.get(1.0, END)
+        decode_message = message.encode("ascii")
+        base64_bytes = base64.b64decode(decode_message)
+        decrypt = base64_bytes.decode("ascii")
+
+        Label(screen2, text="DECRYPT", font="arial", fg="white", bg="#06D6A0").place(x=10, y=0)
+        text2 = Text(screen2, font="Rpbote 10", bg="#D9F6FF", relief=GROOVE, wrap=WORD, bd=0)
+        text2.place(x=10, y=40, width=380, height=150)
+
+        text2.insert(END, decrypt)
+    elif password == "":
+        messagebox.showerror("encryption", "Input Password")
+
+    elif password != "1234":
+        messagebox.showerror("encryption", "Invalid Password")
+
 
 def encrypt():
-    print("")
+    password = code.get()
+
+    if password == "1234":
+        screen1 = Toplevel(screen)
+        screen1.title("encryption")
+        screen1.geometry("400x200")
+        screen1.configure(bg="#EF476F")
+        image_icon = PhotoImage(file="App_icon.png")
+        screen1.iconphoto(False, image_icon)
+
+        message = text1.get(1.0, END)
+        encode_message = message.encode("ascii")
+        base64_bytes = base64.b64encode(encode_message)
+        encrypt = base64_bytes.decode("ascii")
+
+        Label(screen1, text="ENCRYPT", font="arial", fg="white", bg="#EF476F").place(x=10, y=0)
+        text2 = Text(screen1, font="Rpbote 10", bg="#D9F6FF", relief=GROOVE, wrap=WORD, bd=0)
+        text2.place(x=10, y=40, width=380, height=150)
+
+        text2.insert(END, encrypt)
+    elif password=="":
+        messagebox.showerror("encryption","Input Password")
+
+    elif password!="1234":
+        messagebox.showerror("encryption","Invalid Password")
 
 def main_screen():
-    screen=Tk()
+    global screen
+    global code
+    global text1
+
+    screen = Tk()
     screen.geometry("375x398")
+    screen.configure(bg="#073B4C")
 
     #icon
-    image_icon=PhotoImage(file="App_icon.png")
+    image_icon = PhotoImage(file="App_icon.png")
     screen.iconphoto(False, image_icon)
     screen.title("EncDec")
 
@@ -22,19 +77,20 @@ def main_screen():
         code.set("")
         text1.delete(1.0, END)
 
-    Label(text="Enter text for encryption and decryption", fg="black", font=("calbri", 13)).place(x=10, y=10)
-    text1 = Text(font="Robote 20", bg="white", relief=GROOVE, wrap=WORD, bd=0)
+    Label(text="Enter text for encryption and decryption", fg="white", bg="#073B4C", font=("calbri", 13)).place(x=10, y=10)
+    text1 = Text(font="Robote 20", bg="#D9F6FF", relief=GROOVE, wrap=WORD, bd=0)
     text1.place(x=10, y=50, width=355, height=100)
 
-    Label(text="Enter secret key for encryption and decryption", fg="black", font=("calbri", 13)).place(x=10, y=170)
+    Label(text="Enter secret key for encryption and decryption", fg="white",bg="#073B4C", font=("calbri", 13)).place(x=10, y=170)
 
-    code=StringVar()
-    Entry(textvariable=code, width=19,bd=0,font=("arial", 25), show="*").place(x=10, y=200)
+    code = StringVar()
+    Entry(textvariable=code, bg="#D9F6FF", width=19, bd=0, font=("arial", 25), show="*").place(x=10, y=200)
 
-    Button(text="ENCRYPT", height="2", width=23, bg="#ed3833", fg="white", bd=0, command=encrypt).place(x=10, y=250)
-    Button(text="DECRYPT", height="2", width=23, bg="#00bd56", fg="white", bd=0, command=decrypt).place(x=200, y=250)
-    Button(text="RESET", height="2", width=50, bg="#1089ff", fg="white", bd=0, command=reset).place(x=10, y=300)
+    Button(text="ENCRYPT", height="2", width=23, bg="#EF476F", fg="white", bd=0, command=encrypt).place(x=10, y=250)
+    Button(text="DECRYPT", height="2", width=23, bg="#06D6A0", fg="white", bd=0, command=decrypt).place(x=200, y=250)
+    Button(text="RESET", height="2", width=50, bg="#118AB2", fg="white", bd=0, command=reset).place(x=10, y=300)
 
     screen.mainloop()
+
 
 main_screen()
